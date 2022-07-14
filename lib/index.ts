cc4fca0a7e79b5f47b4d9224b29cf9a414200c54
Jason Stallings <octalmage@gmail.com>
2022-07-14 15:01:40 -0500

    fix: Small updates for TypeScript  >= 3.8.
    Also missing semicolons.

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,11 @@
-import { Env } from "@terra-money/terrain";
-import { {{client-name}} } from './clients/{{client-name}}'
+import type { Env } from "@terra-money/terrain";
+import { {{client-name}} } from './clients/{{client-name}}';
 
 export class Lib extends {{client-name}} {
   env: Env;
 
   constructor(env: Env) {
-    super(env.client, env.defaultWallet, env.refs['{{project-name}}'].contractAddresses.default)
+    super(env.client, env.defaultWallet, env.refs['{{project-name}}'].contractAddresses.default);
     this.env = env;
   }
 };
