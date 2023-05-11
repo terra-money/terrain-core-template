51f456f93e6c01a23047eafa8c7acdc35f18311a
plubber <er.h.gorski@gmail.com>
2023-05-11 12:55:33 -0400

    feat: lib reversion to hardcoded client

--- keys.terrain.js
+++ keys.terrain.js
@@ -6,7 +6,4 @@ module.exports = {
     mnemonic:
       "shiver position copy catalog upset verify cheap library enjoy extend second peasant basic kit polar business document shrug pass chuckle lottery blind ecology stand",
   },
-  custom_tester_2: {
-    privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
-  },
 };
--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,23 @@
-import type { Env } from "@terra-money/terrain";
-import { {{client-name}} } from './clients/{{client-name}}';
+import { WaitTxBroadcastResult } from "@terra-money/feather.js";
+import { Env } from "@terra-money/terrain";
 
-export class Lib extends {{client-name}} {
+export class Lib {
   env: Env;
 
-  constructor(env: Env) {
-    super(env.client, env.defaultWallet, env.refs['{{project-name}}'].contractAddresses.default);
+  constructor(env: Env){
     this.env = env;
+  }
+
+  getCount = (env = this.env) => {
+    return env.client.query("{{project-name}}", { get_count: {} })
+  }
+
+  increment = (env = this.env) : Promise<WaitTxBroadcastResult> => {
+    return env.client.execute("{{project-name}}", env.wallets.validator, { increment: {} })
+  }
+  
+  reset = (env = this.env, count: number ) : Promise<WaitTxBroadcastResult> => {
+    return env.client.execute("{{project-name}}", env.wallets.validator, { reset: { count } })
   }
 };
 
